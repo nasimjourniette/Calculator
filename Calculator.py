a729fc2b56c265d2db6a8a3937d3f8888059dfdf
nasimjourniette <128407060+nasimjourniette@users.noreply.github.com>
2023-07-27 12:13:16 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,91 @@
+from xml.etree.ElementTree import TreeBuilder
+
+
+def main():
+    more_operations = True
+    while more_operations:
+        try:
+            if num1 != None:
+                print(f"Your first number is {num1}")
+        except UnboundLocalError:
+            while True:
+                try:
+                    num1 = input("What's your first number? ")
+                    if num1[:-1].isnumeric and num1[-1] == '!' or num1.isnumeric():
+                        pass
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print('please type a valid number')
+                else:
+                    break
+        while True:
+            try:
+                operation = input("What operation are you using on this number? ")
+                if operation not in ('+', '-', '*', '^', '/', '**'):
+                    print('Not a valid operation.')
+                    raise ValueError
+            except ValueError:
+                pass
+            else:
+                break
+        while True:
+            try:
+                num2 = input("What number will you operate with? ")
+                if num2[:-1].isnumeric and num2[-1] == '!' or num2.isnumeric():
+                    pass
+                else:
+                    raise ValueError
+            except ValueError:
+                print('please type a valid number')
+            else:
+                break
+        
+        if '!' == num1[-1]:
+            fact = 1
+            for factorial in range(1, int(num1[:-1]) + 1):
+                fact *= factorial
+            num1 = fact
+        
+        if '!' == num2[-1]:
+            fact2 = 1
+            for factorial in range(1, int(num2[:-1]) + 1):
+                fact2 *= factorial
+            num2 = fact2
+
+        result = float(operate(num1, operation, num2)) 
+        print(f'{num1} {operation} {num2} = {result}')
+        while True:
+            try:
+                again = input(f'Would you like to continue operating with {result}: ("yes", "y") or start a new calculation: ("no", "n") or quit: ("Ctrl + C") ').lower()
+                if again in ('y','yes','no','n'):
+                    if again in ('y', 'yes'):
+                        num1 = str(result)
+                    elif again in ('no', 'n'):
+                        del num1
+                else:
+                    raise ValueError
+            except KeyboardInterrupt:
+                quit()
+            except ValueError:
+                print(f"Type one of these: {'y','yes', 'no', 'n'}.")
+            else:
+                break
+
+
+def operate(n1, operand, n2):
+    n1, n2 = float(n1), float(n2)
+    if operand == '+':
+        return n1 + n2
+    elif operand == '-':
+        return n1 - n2
+    elif operand == '*':
+        return n1 * n2
+    elif operand == '/':
+        return n1 / n2
+    elif operand == '^' or operand == '**':
+        return n1 ** n2
+
+
+if __name__ == '__main__':
+    main()
