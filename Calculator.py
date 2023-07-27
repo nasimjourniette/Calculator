352802da0c2e82d018d91181160946b39c02efea
nasimjourniette <128407060+nasimjourniette@users.noreply.github.com>
2023-07-27 14:17:22 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,12 +2,17 @@ from xml.etree.ElementTree import TreeBuilder
 
 
 def main():
+    # add a loop to continue calculations
     more_operations = True
     while more_operations:
         try:
+            # if num1 does not exist, an UnboundLocalError will be raised and the user can assign num1 to a number.
+            # it's to restart the whole loop after the user is done with their certain calculation and wants to start a new one
+            # else they are continuing with a calculation from a result of one of their previous calculations
             if num1 != None:
                 print(f"Your first number is {num1}")
         except UnboundLocalError:
+            # both numbers can have a factorial symbol after to represent a factorial
             while True:
                 try:
                     num1 = input("What's your first number? ")
@@ -19,6 +24,7 @@ def main():
                     print('please type a valid number')
                 else:
                     break
+        # let user give a valid operation
         while True:
             try:
                 operation = input("What operation are you using on this number? ")
@@ -40,7 +46,7 @@ def main():
                 print('please type a valid number')
             else:
                 break
-        
+        # if a number has a factorial sign then for each number from 1 up to the inputted number multiply it
         if '!' == num1[-1]:
             fact = 1
             for factorial in range(1, int(num1[:-1]) + 1):
@@ -52,9 +58,10 @@ def main():
             for factorial in range(1, int(num2[:-1]) + 1):
                 fact2 *= factorial
             num2 = fact2
-
+        # use the inputted information to find the result of the two numbers
         result = float(operate(num1, operation, num2)) 
         print(f'{num1} {operation} {num2} = {result}')
+        # ask user to decide whether to start with brand new numbers, start with the result, or quit
         while True:
             try:
                 again = input(f'Would you like to continue operating with {result}: ("yes", "y") or start a new calculation: ("no", "n") or quit: ("Ctrl + C") ').lower()
@@ -62,6 +69,7 @@ def main():
                     if again in ('y', 'yes'):
                         num1 = str(result)
                     elif again in ('no', 'n'):
+                        # delete num1 one so when the loop starts again, the UnboundLocalError triggers.
                         del num1
                 else:
                     raise ValueError
